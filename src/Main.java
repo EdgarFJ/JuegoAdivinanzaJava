637b448e8fd9747076d5eed9cf7d803b2132ca0d
EdgarFJ <edjufelipe@gmail.com>
2025-05-04 21:44:04 -0600

    Se hace estructura de bucle

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Random numeroAleatorio = new Random();
+        int numeroGenerado = numeroAleatorio.nextInt(101);
+        Scanner teclado = new Scanner(System.in);
 
+        int intentos = 0;
+
+        while(intentos < 5){
+            System.out.println("Por favor ingrese un numero:");
+            int entradaUsuario = teclado.nextInt();
+
+            if(entradaUsuario == numeroGenerado){
+                System.out.println("El numero coincide");
+                break;
+            }else{
+                if (entradaUsuario < numeroGenerado) {
+                    System.out.println("El número es mayor.");
+                } else {
+                    System.out.println("El número es menor.");
+                }
+            }
+            intentos++;
+            }
+            if (intentos == 5) {
+            System.out.println("Lo siento, se te acabaron los intentos. El número era: " + numeroGenerado);
+        }
     }
 }
